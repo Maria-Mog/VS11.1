d51a37990d20c99ea93e7dc022152a674553495f
Мария <1481047@mail.ru>
2021-11-30 13:21:19 +0300

    Добавьте файлы проекта.

---
+++ VS11.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LQ;
+
+namespace VS11._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LevyeQuation levyeQuation = new LevyeQuation { coefK = 0.45, coefB = 28.56 };
+            levyeQuation.Root();
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
---
+++ VS11.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS11._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LevyeQuation levyeQuation = new LevyeQuation { coefK = 1.45, coefB = -14.25 };
+            levyeQuation.Root();
+            Console.ReadKey();
+        }
+        struct LevyeQuation
+        {
+            public double coefK { get; set; }
+            public double coefB { get; set; }
+
+            public LevyeQuation(double coefK, double coefB)
+            {
+                this.coefK = coefK;
+                this.coefB = coefB;
+            }
+
+            public void Root()
+            {
+                double resultX = -coefB / coefK;
+                Console.WriteLine("Решение уравнения x = {0:f2}", resultX);
+                if (coefK == 0)
+                {
+                    Console.WriteLine("Ошибка, деление на ноль");
+                }
+            }
+        }
+    }
+}
